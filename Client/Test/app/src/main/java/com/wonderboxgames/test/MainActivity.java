404d62730eb74ad2bbd160b7d777acd3e5bb299e
liubenkang <benkang.liu@centurygame.com>
2025-11-22 20:55:14 +0800

    no message

--- Client/Test/app/src/main/java/com/wonderboxgames/test/MainActivity.java
+++ Client/Test/app/src/main/java/com/wonderboxgames/test/MainActivity.java
@@ -3,6 +3,7 @@ package com.wonderboxgames.test;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -12,12 +13,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Activity activityThis = this;
 
+        TextView textView = findViewById(R.id.info);
         findViewById(R.id.btn_test_1)
                 .setOnClickListener(
                         new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 WLog.Log("btn1");
+                                textView.setText("btn1");
                             }
                         }
                 );
@@ -28,6 +31,7 @@ public class MainActivity extends Activity {
                             @Override
                             public void onClick(View v) {
                                 WLog.Log("btn2");
+                                textView.setText("btn2");
                             }
                         }
                 );
@@ -38,6 +42,7 @@ public class MainActivity extends Activity {
                             @Override
                             public void onClick(View v) {
                                 WLog.Log("btn3");
+                                textView.setText("btn3");
                             }
                         }
                 );
@@ -48,6 +53,7 @@ public class MainActivity extends Activity {
                             @Override
                             public void onClick(View v) {
                                 WLog.Log("btn4");
+                                textView.setText("btn4");
                             }
                         }
                 );
